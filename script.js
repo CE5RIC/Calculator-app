dcc4517b6096b092e3459a5edd89a9cdaf8800b0
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-10 12:13:45 +0200

    event listeners and input function created

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Functions for the basic math operators
+
 // Add
 
 function add(a, b) {
@@ -26,7 +28,7 @@ return a / b;
 }
 
 
-// Operator
+// Operator (Function which takes an operator and 2 numbers and then calls one of the above functions on the numbers)
 
 function operate(a, operator, b) {
 if (operator === "+") {
@@ -43,6 +45,57 @@ if (operator === "+") {
     return divided;
 }
 }
+
+
+
+// Event listeners that call the function input when a number gets clicked
+
+document.getElementById('0').addEventListener('click', input);
+document.getElementById("1").addEventListener('click', input);
+document.getElementById('2').addEventListener('click', input);
+document.getElementById('3').addEventListener('click', input);
+document.getElementById('4').addEventListener('click', input);
+document.getElementById('5').addEventListener('click', input);
+document.getElementById('6').addEventListener('click', input);
+document.getElementById('7').addEventListener('click', input);
+document.getElementById('8').addEventListener('click', input);
+document.getElementById('9').addEventListener('click', input);
+document.getElementById('+').addEventListener('click', input);
+document.getElementById('-').addEventListener('click', input);
+document.getElementById('*').addEventListener('click', input);
+document.getElementById('/').addEventListener('click', input);
+document.getElementById('.').addEventListener('click', input);
+
+
+
+
+// 
+
+function input(getElementById) {
+console.log(getElementById.target.innerText);
+let chosenNumber = document.getElementById("display").innerText = getElementById.target.innerText;
+
+console.log(chosenNumber);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
